d570a911eb1769c5e809eee565f5f2f294d57ce5
Selam Gessese <sygessese@gmail.com>
2020-04-19 18:22:14 -0700

    add users to your home feed

--- client/src/components/FollowPopup.js
+++ client/src/components/FollowPopup.js
@@ -2,27 +2,38 @@ import React from "react";
 import { Popup, Button } from "semantic-ui-react";
 import UsersServices from "../services/UsersServices";
 
-const FollowPopup = props => {
-  const startFollowing = () => {
-    UsersServices.followUser(props.id)
+class FollowPopup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: `Click follow to see ${this.props.username}'s activity in your home feed`
+    };
+  }
+
+  startFollowing = () => {
+    UsersServices.followUser(this.props.id)
       .then(s => {
-        console.log(s);
+        this.setState({
+          message: s.data.message
+        });
       })
       .catch(e => {
         console.log(e);
       });
   };
 
-  return (
-    <Popup
-      trigger={<span>{props.username}</span>}
-      hoverable
-      style={{ display: "flex" }}
-    >
-      <p>Click follow to see {props.username}'s activity in your home feed</p>
-      <Button onClick={startFollowing}>Follow</Button>
-    </Popup>
-  );
-};
+  render() {
+    return (
+      <Popup
+        trigger={<span>{this.props.username}</span>}
+        hoverable
+        style={{ display: "flex" }}
+      >
+        <p>{this.state.message}</p>
+        <Button onClick={this.startFollowing.bind(this)}>Follow</Button>
+      </Popup>
+    );
+  }
+}
 
 export default FollowPopup;
